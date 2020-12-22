1c957433f88e54bf94210cf2507879f1ec604fd3
cosensible <cosensible@gmail.com>
2020-12-22 16:14:04 +0800

    99: By Morris

--- LeetCode/99-RecoverTree.h
+++ LeetCode/99-RecoverTree.h
@@ -30,10 +30,50 @@ public:
 	}
 };
 
+class Solution_99_Morris {
+public:
+	void recoverTree(TreeNode* root) {
+		TreeNode *cur = root, *last = nullptr;
+		TreeNode *n1 = nullptr, *n2 = nullptr;
+		while (cur) {
+			if (cur->left) {
+				// 找 cur 节点的前驱节点
+				TreeNode *pre = cur->left;
+				while (pre->right && pre->right != cur) { pre = pre->right; }
+				// 通过前驱节点回到 cur 节点, 处理 cur 节点并还原二叉树, 继续遍历右子树
+				if (pre->right == cur) {
+					if (last && last->val > cur->val) {
+						if (!n1) { n1 = last; }
+						n2 = cur;
+					}
+					pre->right = nullptr;
+					last = cur;
+					cur = cur->right;
+				}
+				// 与前驱节点建立联系, 以便快速回到 cur 节点
+				else {
+					pre->right = cur;
+					cur = cur->left;
+				}
+			}
+			else {
+				if (last && last->val > cur->val) {
+					if (!n1) { n1 = last; }
+					n2 = cur;
+				}
+				last = cur;
+				cur = cur->right;
+			}
+		}
+		swap(n1->val, n2->val);
+	}
+};
+
 void test_99() {
 	vector<const char*> v1 = { "3","1","4","null","null","2" };
 	vector<const char*> v2 = { "2","1","4","null","null","3" };
 	TreeNode *root = arr2Tree(v1), *expt = arr2Tree(v2);
-	Solution_99().recoverTree(root);
+	//Solution_99().recoverTree(root);
+	Solution_99_Morris().recoverTree(root);
 	cout << isTreeSame(root, expt) << endl;
 }
